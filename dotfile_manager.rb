d1c6c5ce130785abfc184e71a038d7e9c465a465
zvkemp <zvkemp@gmail.com>
2018-12-06 12:59:00 -0800

    add helper modules, diffing

--- dotfile_manager.rb
+++ dotfile_manager.rb
@@ -1,6 +1,7 @@
 require 'erb'
 require 'yaml'
 require 'bundler/inline'
+require 'open3'
 
 gemfile do
   gem 'pry-byebug'
@@ -52,9 +53,10 @@ YAMLCONFIG
   # install nvim plugins
   # nvim -c PlugIns -c exit -c exit
 class Configurator
-  def initialize(target)
+  def initialize(target, mods_to_load = [])
     @target = target
     @config = CONFIG['targets'][target]
+    @mods_to_load = mods_to_load
 
     process_config
   end
@@ -63,11 +65,16 @@ class Configurator
 
   def templates
     config['roles'].flat_map do |mod, _|
+      next [] unless should_load?(mod)
       f = File.expand_path("./templates/#{mod}/*.erb")
       Dir[f].map do |template|
         Template.new(template, config: config, mod: mod, target: target)
       end
     end
+  end
+
+  def should_load?(mod)
+    @mods_to_load.empty? || @mods_to_load.include?(mod)
   end
 
   def to_s
@@ -107,15 +114,25 @@ class Template
       if (fm = raw_template[YAML_FRONT_MATTER_REGEXP])
         YAML.load(fm)
       else
-        {} end end
+        {}
+      end
+  end
+
+  def diff
+    Differ.new(self).diff
+  end
 
   def renderable_template
     return raw_template unless (fm = raw_template[YAML_FRONT_MATTER_REGEXP])
     raw_template.lines.drop(fm.lines.count).join
   end
 
+  def to_s
+    @to_s ||= render
+  end
+
   def render
-    RenderContext.new(renderable_template, @config, variables: render_variables).result
+    RenderContext.new(renderable_template, @target, @config, variables: render_variables, helpers: TemplateHelpers::HELPERS[mod]).result
   end
 
   def render_variables
@@ -137,6 +154,7 @@ module HasTarget
     target = name.to_s.sub(/\?\Z/, '')
 
     if ::CONFIG['targets'][target]
+      return false unless target == @target
       return true if query
       yield
     else
@@ -146,31 +164,104 @@ module HasTarget
   end
 end
 
-class RenderContext
-  attr_reader :config, :variables
-
-  def initialize(template, config, variables:)
-    @template = template
-    @config = config
-    @variables = variables
+module TemplateHelpers
+  module Tmux
+    def copy_command
+      case clipboard
+      when 'xclip'
+        'xclip -i -selection clipboard'
+      when 'xsel'
+        'xsel -i --clipboard'
+      when 'pbcopy'
+        'pbcopy'
+      end
+    end
   end
+
+  HELPERS = Hash.new { |h, k| h[k] = [] }
+  HELPERS['tmux'] << Tmux
+end
+
+class RenderContext
+  attr_reader :config, :variables, :template
 
   include HasConfigVariable
   include HasTarget
+
+  def initialize(template, target, config, variables:, helpers:)
+    @template = template
+    @config = config
+    @variables = variables
+    @target = target
+
+    helpers.each { |helper| extend(helper) }
+  end
 
   def result
     ERB.new(@template).result(binding)
   end
 end
 
+class Differ
+  def initialize(template)
+    @template = template
+  end
 
-target = ARGV[0]
+  def diff
+    out, _ = Open3.capture2("diff #{comparison_path} -", stdin_data: @template.to_s)
+    out
+  end
+
+  def diff
+    Open3.pipeline_rw(
+      "diff -U3 --minimal #{comparison_path} -",
+      "sed 's/^-/\x1b[1;31m-/;s/^+/\x1b[1;32m+/;s/^@/\x1b[1;34m@/;s/$/\x1b[0m/'"
+    ) do |stdin, stdout, _thr|
+      stdin.puts(@template.to_s)
+      stdin.close
+
+      stdout.read
+    end
+  end
+
+  def comparison_path
+    File.expand_path("~/#{@template.front_matter['path']}")
+  end
+end
+
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-t", "--target TARGET") do |t|
+    options[:target] = t
+  end
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-d", "--diff", "print diff only") do |v|
+    options[:diff] = v
+  end
+end.parse!
+
+p options
+p ARGV
+mods = ARGV
+target = options[:target]
 raise ArgumentError.new("target `#{target.inspect}` not available") unless target && CONFIG['targets'][target]
 
-c = Configurator.new(target)
+c = Configurator.new(target, mods)
 
 c.templates.each do |t|
-  puts t.render
+  # puts t.render
+  puts t.diff
+
+  # binding.pry
 end
 
 exit(0)
